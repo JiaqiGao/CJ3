9d5016272eee1edf897bcd66908aa2c5fe1bcb1a
cdelfau <cdelfau@stuy.edu>
2016-11-02 12:47:00 -0400

    added db_builder

--- app.py
+++ app.py
@@ -2,22 +2,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 import random, hashlib
 
-import sqlite3
-
-#create the database
-database = "data.db"
-
-#add to the database
-db = sqlite3.connect(database)
-#create a users table
-c.execute("CREATE TABLE users (id INTEGER, username TEXT, password TEXT, dob INTEGER)")
-#create a stories table
-c.execute("CREATE TABLE stories (storyid INTEGER, authorid INTEGER, timestamp INTEGER, content TEXT, last_update TEXT, contributors TEXT, tags TEXT)")
-#commit changes to he database
-db.commit()
-#close the database
-db.close()
-
 #create a Flask app
 app = Flask(__name__)
 
